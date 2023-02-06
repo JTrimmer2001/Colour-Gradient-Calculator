401b2d95f7902da31c26f7b728d80e8fa31f1496
Joshua Trimmer <jet2609@gmail.com>
2023-02-06 16:48:14 +0000

    Removed personal file address

--- fitsToCsv.py
+++ fitsToCsv.py
@@ -5,7 +5,7 @@ from astropy.io import fits
 
 def fitsToCsv(): #Function to convert a fits file to csv format for use in excel or qti or whatever
 
-    File = fits.open("C:/Users/Jet26/Documents/PHYS 369 Code and data/Data/cosmos2015_NOphotometry_masses_sfrs.fits") #Opens fits file, convert to user/specified input at a later date
+    File = fits.open("file name") #Opens fits file, convert to user/specified input at a later date
 
     hdr = File[1].header #astropy.io.fits.header.Header
     dta = File[1].data   #astropy.io.fits.fitsrec.FITS_rec
